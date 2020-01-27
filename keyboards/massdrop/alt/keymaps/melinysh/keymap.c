608411e5ede688f3ec18966045d48646bd7c8046
Stephen Melinyshyn <stephen.melinyshyn@gmail.com>
2020-01-26 22:16:51 -0500

    Airpods Better Touch Tool Toggle

--- keyboards/massdrop/alt/keymaps/melinysh/keymap.c
+++ keyboards/massdrop/alt/keymaps/melinysh/keymap.c
@@ -9,6 +9,7 @@ enum alt_keycodes {
     DBG_MOU,                // DEBUG Toggle Mouse Prints
     MD_BOOT,                // Restart into bootloader after hold timeout
     SND_SLEEP,              // Send sleep ctrl + shit + power to the computer
+    AIRPODS_TGL,            // Send BetterTouchTool Keyboard shortcut to run AppleScript to toggle Airpods connection
 };
 
 // RGB Matrix timeout feature
@@ -28,11 +29,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT, KC_Z,    KC_X, KC_C, KC_V, KC_B, KC_N,   KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN,
         KC_LCTL, KC_LALT, KC_LGUI,                KC_SPC,                         KC_RGUI, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT),
     [1] = LAYOUT(
-        KC_GRV,  KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,    KC_F8,     KC_F9,     KC_F10,  KC_F11,       KC_F12,  _______, KC_MUTE,
-        _______, RGB_SPD,  RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, _______, U_T_AUTO, U_T_AGCR,  _______,   KC_PSCR, KC_SLCK,      KC_PAUS, _______, KC_VOLU,
-        _______, RGB_RMOD, RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, _______, _______,  _______,   SND_SLEEP, _______, _______,               _______, KC_VOLD,
-        _______, RGB_TOG, _______, _______, _______,  MD_BOOT, NK_TOGG, DBG_TOG,  _______,   _______,   _______, _______,               KC_BRIU, _______,
-        _______, _______, _______,                             _______,                      _______,   _______, EEPROM_RESET, KC_BRID, _______),
+        KC_GRV,  KC_F1,    KC_F2,   KC_F3,       KC_F4,    KC_F5,   KC_F6,   KC_F7,    KC_F8,     KC_F9,     KC_F10,  KC_F11,       KC_F12,  _______, _______,
+        _______, RGB_SPD,  RGB_VAI, RGB_SPI,     RGB_HUI,  RGB_SAI, _______, U_T_AUTO, U_T_AGCR,  _______,   KC_PSCR, KC_SLCK,      KC_PAUS, _______, KC_MUTE,
+        _______, RGB_RMOD, RGB_VAD, RGB_MOD,     RGB_HUD,  RGB_SAD, _______, _______,  _______,   SND_SLEEP, _______, _______,               _______, KC_VOLU,
+        _______, RGB_TOG, _______,  AIRPODS_TGL, _______,  MD_BOOT, NK_TOGG, DBG_TOG,  _______,   _______,   _______, _______,               KC_BRIU, KC_VOLD,
+        _______, _______, _______,                                  _______,                      _______,   _______, EEPROM_RESET, KC_BRID, _______),
     /* 
     [X] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
@@ -152,6 +153,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 SEND_STRING(SS_DOWN(X_LGUI) SS_DOWN(X_LCTRL) SS_DOWN(X_Q) SS_UP(X_LGUI) SS_UP(X_LCTRL) SS_UP(X_Q));
                 rgb_matrix_disable_noeeprom();
                 rgb_matrix_on = false;
+            }
+            return false;
+        case AIRPODS_TGL:
+            if (record->event.pressed) {
+                SEND_STRING(SS_DOWN(X_LGUI) SS_DOWN(X_LCTRL) SS_DOWN(X_LALT) SS_DOWN(X_LSHIFT) SS_DOWN(X_C) SS_UP(X_LGUI) SS_UP(X_LCTRL) SS_UP(X_LALT) SS_UP(X_LSHIFT) SS_UP(X_C));
             }
             return false;
         default:
